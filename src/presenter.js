bec7b32441d22f157e43835908a37f6934d5e67f
Salma Torres <tsalma569@gmail.com>
2024-02-14 22:41:18 -0400

    Segundo commit, nueva funcion multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -13,3 +13,19 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+import multiplicar from "./multiplicador";
+
+first = document.querySelector("#n1");
+second = document.querySelector("#n2");
+form = document.querySelector("#multiplicar-form");
+div = document.querySelector("#resultado-mult");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+});
