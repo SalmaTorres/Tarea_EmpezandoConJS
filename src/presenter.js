70a87aa9bf0517a5f11024499ccf15706da98404
Salma Torres <tsalma569@gmail.com>
2024-02-14 22:44:52 -0400

    Nueva funcion multiplicar, 2do intento

--- src/presenter.js
+++ src/presenter.js
@@ -16,16 +16,16 @@ form.addEventListener("submit", (event) => {
 
 import multiplicar from "./multiplicador";
 
-first = document.querySelector("#n1");
-second = document.querySelector("#n2");
-form = document.querySelector("#multiplicar-form");
-div = document.querySelector("#resultado-mult");
+const num1 = document.querySelector("#n1");
+const num2 = document.querySelector("#n2");
+const formulario = document.querySelector("#multiplicar-form");
+const resul = document.querySelector("#resultado-mult");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+formulario.addEventListener("submit", (event) => {
+  resul.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(num1.value);
+  const secondNumber = Number.parseInt(num2.value);
 
   div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
