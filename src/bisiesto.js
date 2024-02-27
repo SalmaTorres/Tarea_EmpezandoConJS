d76879c8cf83962a474d006432955a18f5635d68
Salma Torres <tsalma569@gmail.com>
2024-02-26 23:10:11 -0400

    Se genera NO ES BISIESTO si el anio es divisible por 100 y no por 400

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,7 +1,12 @@
 function EsBisiesto(anio){
     if(anio % 400 == 0){
         return "ES BISIESTO";
+    }else{
+        if(anio % 100 == 0){
+            return "NO ES BISIESTO";
+        }
     }
+    return " ";
 }
 
 export default EsBisiesto;
--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -7,4 +7,10 @@ describe("Bisiesto", () => {
     it("deberia generar ES BISIESTO si el anio es divisible por 400", () => {
         expect(EsBisiesto(2000)).toEqual("ES BISIESTO");
     });
+    it("deberia generar NO ES BISIESTO si el anio es divisible por 100 y no por 400", () => {
+        expect(EsBisiesto(1700)).toEqual("NO ES BISIESTO");
+    });
+    it("deberia generar NO ES BISIESTO si el anio es divisible por 100 y no por 400", () => {
+        expect(EsBisiesto(1900)).toEqual("NO ES BISIESTO");
+    });
 })
