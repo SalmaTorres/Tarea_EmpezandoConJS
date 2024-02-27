c90bba18203c486a67b42455d0418bbc8a30c185
Salma Torres <tsalma569@gmail.com>
2024-02-26 21:10:01 -0400

    Se genera el mismo numero si no tiene reglas

--- src/fizzbazz.spec.js
+++ src/fizzbazz.spec.js
@@ -4,4 +4,7 @@ describe("Fizzbazz", () => {
     it("deberia generar el mismo numero para uno que no tiene reglas como el nro 1", () => {
         expect(generarFizzbazz(1)).toEqual("1");
     });
+    it("deberia generar el mismo numero para uno que no tiene reglas como el nro 2", () => {
+        expect(generarFizzbazz(2)).toEqual("2");
+    });
 })
