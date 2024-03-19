56a932c9b149aba57a358238affa2c23e38af590
Salma Torres <tsalma569@gmail.com>
2024-03-19 00:01:24 -0400

    Refactorizacion

--- src/BowlingGameTest.test.js
+++ src/BowlingGameTest.test.js
@@ -1,19 +1,24 @@
 import Game from "./BowlingGame.js";
 
 describe("BowlingGame", () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it("debería crear una instancia de Game", () => {
-    const game = new Game();
     expect(game instanceof Game).toEqual(true);
   });
+
   it("debería devolver 0 para un juego sin lanzamientos", () => {
-    const game = new Game();
     for (let i = 0; i < 20; i++) {
       game.roll(0);
     }
     expect(game.score()).toEqual(0);
   });
-  it("debería devolver 20 para un juego de todos 1 ", () => {
-    const game = new Game();
+
+  it("debería devolver 20 para un juego de todos 1", () => {
     for (let i = 0; i < 20; i++) {
       game.roll(1);
     }
